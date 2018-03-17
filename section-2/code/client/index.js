a910c014d06a1844054195bd7f3d5af1c2e20f7c
Joel Martinez <jmgalanes@gmail.com>
2018-03-17 13:09:15 -0400

    Fixing js errors

--- section-2/code/client/index.js
+++ section-2/code/client/index.js
@@ -1,27 +1,13 @@
-// const Web3 = require('web3')
-// const contract = require('truffle-contract')
-//
-// const web3 = new Web3(new Web3.providers.HttpProvider('	https://rinkeby.infura.io/npPr7wL0YRxP3ewG82AL'))
-// const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/npPr7wL0YRxP3ewG82AL')
-//
-// var MyContract = contract({
-//   abi: '',
-//   address: '0xe9a6ae1086305af6ca9819daaa03ec2983a3fe18', // optional
-//   // many more
-// })
-//
-// MyContract.setProvider(provider)
-//
-//
+const Web3 = require('web3')
 const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/npPr7wL0YRxP3ewG82AL')
 const artifacts = require('../build/contracts/SimpleSensorMarket.json')
 const contract = require('truffle-contract')
 let SimpleSensorMarket = contract(artifacts)
 SimpleSensorMarket.setProvider(provider)
-const SimpleSensorMarket.deployed()
+SimpleSensorMarket.deployed()
   .then(function (instance) {
     console.log(instance)
   })
-  .then(function (tx_id) {
-    console.log(tx_id)
+  .then(function (txId) {
+    console.log(txId)
   })
